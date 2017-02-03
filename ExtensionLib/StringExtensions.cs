5c598cb9064972586f73dcb7cb5b931c3a68f86b
Johnny Olsa <johnny@johnnyolsa.com>
2017-02-03 15:12:47 -0600

    Enhanced diacritics

--- ExtensionLib/StringExtensions.cs
+++ ExtensionLib/StringExtensions.cs
@@ -5,15 +5,23 @@ using System.Linq;
 
 namespace ExtensionLib
 {
-    public static class StringExtensions
-    {
-        public static bool IgnoreDiacriticsContains(this string value, string search)
-        {
-            return CultureInfo.CurrentCulture.CompareInfo.IndexOf(value, search, CompareOptions.IgnoreCase | CompareOptions.IgnoreNonSpace) > -1;
-        }
-        public static bool IgnoreDiacriticsEquals(this string value, string other)
-        {
-            return CultureInfo.CurrentCulture.CompareInfo.Compare(value, other, CompareOptions.IgnoreCase | CompareOptions.IgnoreNonSpace) == 0;
-        }
-    }
+	public static class StringExtensions
+	{
+		public static int IgnoreDiacriticsIndexOf(this string value, string search)
+		{
+			if (value == null || search == null)
+				return value == search ? 0 : -1;
+			return CultureInfo.CurrentCulture.CompareInfo.IndexOf(value, search, CompareOptions.IgnoreCase | CompareOptions.IgnoreNonSpace);
+		}
+		public static bool IgnoreDiacriticsContains(this string value, string search)
+		{
+			return IgnoreDiacriticsIndexOf(value, search) > -1;
+		}
+		public static bool IgnoreDiacriticsEquals(this string value, string other)
+		{
+			if (value == null || other == null)
+				return value == other;
+			return CultureInfo.CurrentCulture.CompareInfo.Compare(value, other, CompareOptions.IgnoreCase | CompareOptions.IgnoreNonSpace) == 0;
+		}
+	}
 }
